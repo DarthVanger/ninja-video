55a11cdc930875531315cd9d3edc0e8ab2f4b165
DarthVanger <darthvanger@gmail.com>
2020-05-07 04:25:07 +0300

    Improve audio sync for dance and make it longer

--- audio/cut.sh
+++ audio/cut.sh
@@ -20,15 +20,15 @@ ffmpeg -y -ss 00:00:00 -i "output/01-story.mp3" "${output_folder}/01-story.mp3"
 ffmpeg -y -ss 00:00:00 -i "output/02-story.mp3" "${output_folder}/02-story.mp3"
 
 # silence from 7 sec to 28
-ffmpeg -y -ss 00:00:00 -i "output/silence.mp3" -t 21 "${output_folder}/03-silence.mp3" 
+ffmpeg -y -ss 00:00:00 -i "output/silence.mp3" -t 21 "${output_folder}/03-silence.mp3"
 
 ## story 2
-ffmpeg -y -ss 00:00:00 -i "output/03-story.mp3" -t 21 "${output_folder}/04-story.mp3" 
-ffmpeg -y -ss 00:00:00 -i "output/04-story.mp3" -t 21 "${output_folder}/05-story.mp3" 
-ffmpeg -y -ss 00:00:00 -i "output/05-story.mp3" -t 21 "${output_folder}/06-story.mp3" 
+ffmpeg -y -ss 00:00:00 -i "output/03-story.mp3" -t 21 "${output_folder}/04-story.mp3"
+ffmpeg -y -ss 00:00:00 -i "output/04-story.mp3" -t 21 "${output_folder}/05-story.mp3"
+ffmpeg -y -ss 00:00:00 -i "output/05-story.mp3" -t 21 "${output_folder}/06-story.mp3"
 
 ## silence from 00:42 to 01:01
-ffmpeg -y -ss 00:00:00 -i "output/silence.mp3" -t 19.5 "${output_folder}/07-silence.mp3" 
+ffmpeg -y -ss 00:00:00 -i "output/silence.mp3" -t 19.2 "${output_folder}/07-silence.mp3"
 
 ## ninja track for dance :)
-ffmpeg -y -ss 00:02:51.5 -i "${source_folder}/ninja.mp3" -t 25 "${output_folder}/08-ninja.mp3" 
+ffmpeg -y -ss 00:02:51.2 -i "${source_folder}/ninja.mp3" -t 40 "${output_folder}/08-ninja.mp3"
