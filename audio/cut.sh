eb86484e8059a88fdb03e2c9bc12547114c30287
DarthVanger <darthvanger@gmail.com>
2020-05-07 04:15:05 +0300

    Add ninja track for dance!!! :)

--- audio/cut.sh
+++ audio/cut.sh
@@ -26,3 +26,9 @@ ffmpeg -y -ss 00:00:00 -i "output/silence.mp3" -t 21 "${output_folder}/03-silenc
 ffmpeg -y -ss 00:00:00 -i "output/03-story.mp3" -t 21 "${output_folder}/04-story.mp3" 
 ffmpeg -y -ss 00:00:00 -i "output/04-story.mp3" -t 21 "${output_folder}/05-story.mp3" 
 ffmpeg -y -ss 00:00:00 -i "output/05-story.mp3" -t 21 "${output_folder}/06-story.mp3" 
+
+## silence from 00:42 to 01:01
+ffmpeg -y -ss 00:00:00 -i "output/silence.mp3" -t 19.5 "${output_folder}/07-silence.mp3" 
+
+## ninja track for dance :)
+ffmpeg -y -ss 00:02:51.5 -i "${source_folder}/ninja.mp3" -t 25 "${output_folder}/08-ninja.mp3" 
