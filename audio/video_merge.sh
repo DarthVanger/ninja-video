ad08733b7132bff76d0dc269013c2cba8a478d84
DarthVanger <darthvanger@gmail.com>
2020-05-07 03:21:23 +0300

    Mix video audio and overlayed audio

--- audio/video_merge.sh
+++ audio/video_merge.sh
@@ -3,4 +3,11 @@ source ~/.bash_profile
 video=../output/video.mp4
 audio=output/merged.mp3
 
-ffmpeg -y -i "$video" -i "$audio" -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 output/video.mp4
+audio_from_video="output/audio-from-video.mp3"
+mixed_audio="output/mixed-audio.mp3"
+
+ffmpeg -y -i "$video" -q:a 0 -map a "$audio_from_video"
+
+ffmpeg -y -i "$audio_from_video" -i "$audio" -filter_complex amix=inputs=2:duration=longest "$mixed_audio"
+
+ffmpeg -y -i "$video" -i "$mixed_audio" -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 output/video.mp4
